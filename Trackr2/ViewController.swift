f97f172c521052304378cfca964bcde4277b0f46
Matteo Comisso <matteo.comisso@me.com>
2017-05-18 23:55:57 +0200

    Initial Commit

---
+++ Trackr2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trackr2
+//
+//  Created by Matteo Comisso on 18/05/2017.
+//  Copyright © 2017 mcomisso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
