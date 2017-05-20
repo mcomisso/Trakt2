08a95d4e544f9ab4ed747e7d15ff439bc3907733
Matteo Comisso <matteo.comisso@me.com>
2017-05-20 12:51:49 +0200

    Implementation of DataSource in movie list

--- Trackr2/MCMovieListTableViewController.swift
+++ Trackr2/MCMovieListTableViewController.swift
@@ -28,20 +28,19 @@ class MCMovieListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.movies.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: R.reuseIdentifier.movieCell.identifier, for: indexPath) as? MCMovieTableViewCell else {
+            fatalError()
+        }
 
-        // Configure the cell...
+        cell.setMovie(movie: self.movies[indexPath.row])
 
         return cell
     }
