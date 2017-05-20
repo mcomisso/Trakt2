dacabe209a9e3b98c76d0114b4d413193567371a
Matteo Comisso <matteo.comisso@me.com>
2017-05-20 12:53:39 +0200

    Initial DetailsViewController class

--- Trackr2/MCDetailsViewController.swift
+++ Trackr2/MCDetailsViewController.swift
@@ -8,19 +8,18 @@
 
 import UIKit
 
-class MCDetailsViewController: UIViewController {
+final class MCDetailsViewController: UIViewController {
+
+    var movie: Movie!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setMovie(_ movie: Movie) {
+        self.movie = movie
     }
-    
+
 
     /*
     // MARK: - Navigation
