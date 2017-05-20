dc163a408804a54ea08c34de65f7e972c8aba17e
Matteo Comisso <matteo.comisso@me.com>
2017-05-20 17:41:58 +0200

    [Refactor] removed segue code from detailsVC

--- Trackr2/MCDetailsViewController.swift
+++ Trackr2/MCDetailsViewController.swift
@@ -20,15 +20,6 @@ final class MCDetailsViewController: UIViewController {
         self.movie = movie
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
