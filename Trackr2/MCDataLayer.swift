dc3f92c40cfc162a1bc56a31b20ed80bdac565d5
Matteo Comisso <matteo.comisso@me.com>
2017-05-21 12:32:07 +0200

    [Refactor] Moved public logic to extension

--- Trackr2/MCDataLayer.swift
+++ Trackr2/MCDataLayer.swift
@@ -98,12 +98,42 @@ final class MCDataLayer {
             throw SerializableError.saveFile
         }
     }
+extension MCDataLayer {
 
 
-    func listMovies() -> [Movie] {
-        return self.movies
+    func listDetailsForMovie(movie: Movie, completion: (Bool, TMDBMovie)->Void) {
+
     }
 
+    func listMovies(completion: @escaping MovieCompletion)  {
+
+        if self.movies.isEmpty {
+            // Network request
+
+            self.traktAPI.request(endpoint: MCTrackrEndpoint.trendingMovies()).response { [weak self] resp in
+                guard let strongSelf = self else { return }
+
+                if let error = resp.error {
+                    print(error.localizedDescription)
+                    completion(false, [])
+                } else {
+                    if let data = resp.data {
+                        let json = JSON(data: data)
+                        for movieJSON in json.arrayValue {
+                            let movie = try! Movie(json: movieJSON)
+                            strongSelf.movies.append(movie)
+                        }
+
+                        completion(true, strongSelf.movies)
+                        try! strongSelf.save(strongSelf.movies)
+                    }
+                }
+            }
+
+        } else {
+            completion(true, self.movies)
+        }
+    }
 
 
     public func readTMDBConfiguration(completion: @escaping Completion) {
