6b6977e3f09e7fff854af5ba944c446354dc5d88
Matteo Comisso <matteo.comisso@me.com>
2017-05-24 03:23:34 +0200

    Extension to Serialize native TMDSwift configuration struct

--- Trackr2/MCModel.swift
+++ Trackr2/MCModel.swift
@@ -10,29 +10,51 @@ import Foundation
 import SwiftyJSON
 import TMDBSwift
 
+extension ConfigurationMDB: SerializableStruct {
     var asDictionary: StandardDict {
         get {
             return [
-                "baseUrl": self.baseUrl
+                "base_url": self.base_url,
+                "secure_base_url": self.secure_base_url,
+                "backdrop_sizes": self.backdrop_sizes,
+                "poster_sizes": self.poster_sizes,
+                "profile_sizes": self.profile_sizes,
+                "logo_sizes": self.logo_sizes,
+                "change_keys": self.change_keys,
+                "still_sizes": self.still_sizes
             ]
         }
     }
 
-    init(data: StandardDict) {
-        guard let baseURL = data["baseUrl"] as? String,
-            let secureBaseUrl = data["secureBaseUrl"] as? String,
-            let backdropSizes = data["backdropSizes"] as? [String],
-            let posterSizes = data["posterSizes"] as? [String],
-            let profileSizes = data["profileSizes"] as? [String],
-            let logoSizes = data["logoSizes"] as? [String]
-            else { fatalError() }
+    init?(data: StandardDict) {
+        guard let baseURL = data["base_url"] as? String,
+            let secureBaseUrl = data["secure_base_url"] as? String,
+            let backdropSizes = data["backdrop_sizes"] as? [String],
+            let posterSizes = data["poster_sizes"] as? [String],
+            let profileSizes = data["profile_sizes"] as? [String],
+            let logoSizes = data["logo_sizes"] as? [String],
+            let change_keys = data["change_keys"] as? [String],
+            let still_sizes = data["still_sizes"] as? [String]
+            else { return nil }
 
-        self.baseUrl = baseURL
-        self.secureBaseUrl = secureBaseUrl
-        self.backdropSizes = backdropSizes
-        self.posterSizes = posterSizes
-        self.profileSizes = profileSizes
-        self.logoSizes = logoSizes
+        self.base_url = baseURL
+        self.secure_base_url = secureBaseUrl
+        self.backdrop_sizes = backdropSizes
+        self.poster_sizes = posterSizes
+        self.profile_sizes = profileSizes
+        self.logo_sizes = logoSizes
+        self.change_keys = change_keys
+        self.still_sizes = still_sizes
+    }
+
+
+    func generatePosterUrl(secure: Bool, dimension: TMDBImageDimension = .big) -> URL {
+        guard let baseURL = secure ? URL(string: self.secure_base_url) : URL(string: self.base_url),
+        let size = dimension == .small ? self.poster_sizes.first : self.poster_sizes.last else { fatalError() }
+        return baseURL.appendingPathComponent(size)
     }
 }
 
+enum TMDBImageDimension {
+    case small, big
+}
