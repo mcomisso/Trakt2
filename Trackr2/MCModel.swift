7513b0c97c8c74991f5c3685b9cd015de28e2303
Matteo Comisso <matteo.comisso@me.com>
2017-05-24 03:23:02 +0200

    Using TMDBSwift library

--- Trackr2/MCModel.swift
+++ Trackr2/MCModel.swift
@@ -8,28 +8,7 @@
 
 import Foundation
 import SwiftyJSON
-
-struct TMDBConfiguration: SerializableStruct {
-    let baseUrl: String
-    let secureBaseUrl: String
-
-    let backdropSizes: [String]
-    let logoSizes: [String]
-    let posterSizes: [String]
-    let profileSizes: [String]
-
-
-    // Other keys unused
-
-    init(json: JSON) throws {
-        let images = json["images"]
-        self.baseUrl = images["base_url"].stringValue
-        self.secureBaseUrl = images["secure_base_url"].stringValue
-        self.backdropSizes = images["backdrop_sizes"].arrayValue.map { $0.stringValue }
-        self.logoSizes = images["logo_sizes"].arrayValue.map { $0.stringValue }
-        self.posterSizes = images["poster_sizes"].arrayValue.map { $0.stringValue }
-        self.profileSizes = images["profile_sizes"].arrayValue.map { $0.stringValue }
-    }
+import TMDBSwift
 
     var asDictionary: StandardDict {
         get {
