691f3484aedb30e9ad5a616fcf658326481e8772
Matteo Comisso <matteo.comisso@me.com>
2017-05-24 03:24:13 +0200

    Prepare for reuse improvements

--- Trackr2/MCMovieTableViewCell.swift
+++ Trackr2/MCMovieTableViewCell.swift
@@ -35,14 +35,10 @@ class MCMovieTableViewCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
     override func prepareForReuse() {
         super.prepareForReuse()
+        self.coverImage.image = nil
+        self.movieTitle.text = nil
         self.coverImage.af_cancelImageRequest()
     }
 
